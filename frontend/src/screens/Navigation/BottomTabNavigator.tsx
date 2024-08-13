434cb9973f1e7c444ee48f25668eec803caff842
SeonSukyeong <seon@sookmyung.ac.kr>
2024-08-13 22:34:46 +0900

    Initial commit

---
+++ frontend/src/screens/Navigation/BottomTabNavigator.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home/homescreen';
+import Search from '../screens/Search/searchscreen';
+import My from '../screens/MY/myscreen';
+import Vote from '../screens/Vote/VoteScreen';
+
+const homeIcon = require('../img/home.png');
+const searchIcon = require('../img/search.png');
+const myIcon = require('../img/my.png');
+const voteIcon = require('../img/create.png');
+const logoTitle = require('../img/logoTitle.png');
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconSource;
+
+          if (route.name === 'Home') {
+            iconSource = homeIcon;
+          } else if (route.name === 'Search') {
+            iconSource = searchIcon;
+          } else if (route.name === 'My') {
+            iconSource = myIcon;
+          } else if (route.name === 'Vote') {
+            iconSource = voteIcon;
+          }
+
+          return (
+            <Image
+              source={iconSource}
+              style={{width: size, height: size, tintColor: color}}
+            />
+          );
+        },
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+        headerTitle: () => (
+          <Image
+            source={logoTitle}
+            style={{width: 150, height: 50}}
+            resizeMode="contain"
+          />
+        ),
+        headerTitleAlign: 'center',
+      })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Vote" component={Vote} />
+      <Tab.Screen name="My" component={My} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
